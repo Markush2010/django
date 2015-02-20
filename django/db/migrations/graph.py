ef7a53d1e706e129e4727f34b82f31d5987d08ab
Marten Kenbeek <marten.knbk@gmail.com>
2015-02-20 05:43:45 +0100

    [WIP] #24366 -- Optimized generation of migration dependency graphs.

--- django/db/migrations/graph.py
+++ django/db/migrations/graph.py
@@ -8,6 +8,57 @@ from django.utils.encoding import python_2_unicode_compatible
 
 
 @python_2_unicode_compatible
+class Node(object):
+    __slots__ = ['key', 'app_label', 'migration_name', 'migration', 'children', 'parents', 'descendants', 'ancestors']
+
+    def __init__(self, key, migration):
+        self.key = key
+        self.app_label = key[0]
+        self.migration_name = key[1]
+        self.migration = migration
+        self.children = set()
+        self.parents = set()
+
+    def __eq__(self, other):
+        return (isinstance(other, Node) and self.app_label == other.app_label and self.migration_name == other.migration_name
+                or isinstance(other, tuple) and self.app_label == other[0] and self.migration_name == other[1])
+
+    def __hash__(self):
+        return hash((self.app_label, self.migration_name))
+
+    def __getitem__(self, item):
+        return self.key[item]
+
+    def add_parent(self, parent):
+        self.parents.add(parent)
+
+    def add_child(self, child):
+        self.children.add(child)
+
+    def get_descendants(self):
+        if not hasattr(self, 'descendants'):
+            descendants = deque([self])
+            for child in sorted(self.children):
+                descendants.extendleft(reversed(child.get_descendants()))
+            self.descendants = list(OrderedSet(descendants))
+        return self.descendants
+
+    def get_ancestors(self):
+        if not hasattr(self, 'ancestors'):
+            ancestors = deque([self])
+            for parent in sorted(self.parents):
+                ancestors.extendleft(reversed(parent.get_ancestors()))
+            self.ancestors = list(OrderedSet(ancestors))
+        return self.ancestors
+
+    def __str__(self):
+        return str((self.app_label, self.migration_name))
+
+    def __repr__(self):
+        return '<Node: (%s, %s)>' % (self.app_label, self.migration_name)
+
+
+@python_2_unicode_compatible
 class MigrationGraph(object):
     """
     Represents the digraph of all migrations in a project.
@@ -32,26 +83,39 @@ class MigrationGraph(object):
     """
 
     def __init__(self):
+        self.node_map = {}
         self.nodes = {}
-        self.dependencies = {}
-        self.dependents = {}
+        self.cached = False
 
-    def add_node(self, node, implementation):
+    def add_node(self, key, implementation):
+        node = Node(key, implementation)
+        self.node_map[key] = node
         self.nodes[node] = implementation
+        self.clear_cache()
 
     def add_dependency(self, migration, child, parent):
-        if child not in self.nodes:
+        if child not in self.node_map:
             raise NodeNotFoundError(
                 "Migration %s dependencies reference nonexistent child node %r" % (migration, child),
                 child
             )
-        if parent not in self.nodes:
+        if parent not in self.node_map:
             raise NodeNotFoundError(
                 "Migration %s dependencies reference nonexistent parent node %r" % (migration, parent),
                 parent
             )
-        self.dependencies.setdefault(child, set()).add(parent)
-        self.dependents.setdefault(parent, set()).add(child)
+        self.node_map[child].add_parent(self.node_map[parent])
+        self.node_map[parent].add_child(self.node_map[child])
+        self.clear_cache()
+
+    def clear_cache(self):
+        if self.cached:
+            for node in self.nodes:
+                if hasattr(node, 'ancestors'):
+                    del node.ancestors
+                if hasattr(node, 'descendants'):
+                    del node.descendants
+            self.cached = False
 
     def forwards_plan(self, node):
         """
@@ -62,7 +126,9 @@ class MigrationGraph(object):
         """
         if node not in self.nodes:
             raise NodeNotFoundError("Node %r not a valid node" % (node, ), node)
-        return self.dfs(node, lambda x: self.dependencies.get(x, set()))
+        self.ensure_not_cyclic(node, lambda x: self.node_map[x].parents)
+        self.cached = True
+        return self.node_map[node].get_ancestors()
 
     def backwards_plan(self, node):
         """
@@ -73,7 +139,9 @@ class MigrationGraph(object):
         """
         if node not in self.nodes:
             raise NodeNotFoundError("Node %r not a valid node" % (node, ), node)
-        return self.dfs(node, lambda x: self.dependents.get(x, set()))
+        self.ensure_not_cyclic(node, lambda x: self.node_map[x].children)
+        self.cached = True
+        return self.node_map[node].get_descendants()
 
     def root_nodes(self, app=None):
         """
@@ -82,8 +150,8 @@ class MigrationGraph(object):
         """
         roots = set()
         for node in self.nodes:
-            if (not any(key[0] == node[0] for key in self.dependencies.get(node, set()))
-                    and (not app or app == node[0])):
+            if (not any(key.app_label == node.app_label for key in list(node.parents))
+                    and (not app or app == node.app_label)):
                 roots.add(node)
         return sorted(roots)
 
@@ -97,8 +165,8 @@ class MigrationGraph(object):
         """
         leaves = set()
         for node in self.nodes:
-            if (not any(key[0] == node[0] for key in self.dependents.get(node, set()))
-                    and (not app or app == node[0])):
+            if (not any(key.app_label == node.app_label for key in list(node.children))
+                    and (not app or app == node.app_label)):
                 leaves.add(node)
         return sorted(leaves)
 
@@ -143,7 +211,7 @@ class MigrationGraph(object):
     def __str__(self):
         return "Graph: %s nodes, %s edges" % (
             len(self.nodes),
-            sum(len(x) for x in self.dependencies.values()),
+            sum(len(node.parents) for node in self.nodes),
         )
 
     def make_state(self, nodes=None, at_end=True, real_apps=None):
@@ -156,7 +224,7 @@ class MigrationGraph(object):
             nodes = list(self.leaf_nodes())
         if len(nodes) == 0:
             return ProjectState()
-        if not isinstance(nodes[0], tuple):
+        if not isinstance(nodes[0], (tuple, Node)):
             nodes = [nodes]
         plan = []
         for node in nodes:
