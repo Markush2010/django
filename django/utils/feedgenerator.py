9acc50df64408d4023378ae4259fa1069b215ab2
Unai Zalakain <unai@gisa-elkartea.org>
2015-08-21 11:50:43 +0200

    Working on #13110: Accept multiple enclosures for Atom feeds

--- django/utils/feedgenerator.py
+++ django/utils/feedgenerator.py
@@ -117,12 +117,13 @@ class SyndicationFeed(object):
 
     def add_item(self, title, link, description, author_email=None,
             author_name=None, author_link=None, pubdate=None, comments=None,
-            unique_id=None, unique_id_is_permalink=None, enclosure=None,
+            unique_id=None, unique_id_is_permalink=None, enclosures=None,
             categories=(), item_copyright=None, ttl=None, updateddate=None, **kwargs):
         """
         Adds an item to the feed. All args are expected to be Python Unicode
         objects except pubdate and updateddate, which are datetime.datetime
-        objects, and enclosure, which is an instance of the Enclosure class.
+        objects, and enclosures which is an iterable of instances of the
+        Enclosure class.
         """
         to_unicode = lambda s: force_text(s, strings_only=True)
         if categories:
@@ -130,6 +131,8 @@ class SyndicationFeed(object):
         if ttl is not None:
             # Force ints to unicode
             ttl = force_text(ttl)
+        if enclosures is None:
+            enclosures = []
         item = {
             'title': to_unicode(title),
             'link': iri_to_uri(link),
@@ -142,7 +145,7 @@ class SyndicationFeed(object):
             'comments': to_unicode(comments),
             'unique_id': to_unicode(unique_id),
             'unique_id_is_permalink': unique_id_is_permalink,
-            'enclosure': enclosure,
+            'enclosures': enclosures,
             'categories': categories or (),
             'item_copyright': to_unicode(item_copyright),
             'ttl': ttl,
@@ -317,10 +320,16 @@ class Rss201rev2Feed(RssFeed):
             handler.addQuickElement("ttl", item['ttl'])
 
         # Enclosure.
-        if item['enclosure'] is not None:
+        if item['enclosures']:
+            enclosures = list(item['enclosures'])
+            if len(enclosures) > 1:
+                raise ValueError("RSS feed items may only have one enclosure, "
+                                 "see http://www.rssboard.org/rss-profile#element-channel-item-enclosure")
+            enclosure = enclosures[0]
             handler.addQuickElement("enclosure", '',
-                {"url": item['enclosure'].url, "length": item['enclosure'].length,
-                    "type": item['enclosure'].mime_type})
+                                    {"url": enclosure.url,
+                                     "length": enclosure.length,
+                                     "type": enclosure.mime_type})
 
         # Categories.
         for cat in item['categories']:
@@ -328,7 +337,7 @@ class Rss201rev2Feed(RssFeed):
 
 
 class Atom1Feed(SyndicationFeed):
-    # Spec: http://atompub.org/2005/07/11/draft-ietf-atompub-format-10.html
+    # Spec: https://tools.ietf.org/html/rfc4287
     content_type = 'application/atom+xml; charset=utf-8'
     ns = "http://www.w3.org/2005/Atom"
 
@@ -405,13 +414,13 @@ class Atom1Feed(SyndicationFeed):
         if item['description'] is not None:
             handler.addQuickElement("summary", item['description'], {"type": "html"})
 
-        # Enclosure.
-        if item['enclosure'] is not None:
+        # Enclosures.
+        for enclosure in item.get('enclosures') or []:
             handler.addQuickElement("link", '',
-                {"rel": "enclosure",
-                 "href": item['enclosure'].url,
-                 "length": item['enclosure'].length,
-                 "type": item['enclosure'].mime_type})
+                                    {"rel": "enclosure",
+                                    "href": enclosure.url,
+                                    "length": enclosure.length,
+                                    "type": enclosure.mime_type})
 
         # Categories.
         for cat in item['categories']:
